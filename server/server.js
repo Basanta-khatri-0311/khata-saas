03fffbb3e9bf5d981e925dea17de448882751d93
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2026-04-02 16:46:39 +0545

    feat: update server configuration and database connection setup

--- server/server.js
+++ server/server.js
@@ -1,5 +1,11 @@
-const express = require("express");
-const cors = require("cors");
+const express = require("express"); //for creating the server
+const cors = require("cors"); //for handling cross-origin requests
+const dotenv = require("dotenv"); //for loading environment variables
+const connectDB = require("./config/db"); //for connecting to the database
+
+
+dotenv.config(); // Load environment variables from .env file
+connectDB(); // Connect to the database
 
 const app = express();
 
@@ -8,7 +14,7 @@ app.use(cors()); //for cross-origin requests
 app.use(express.json()); //for parsing application/json
 
 
-const PORT = process.env.PORT || 5500;
+const PORT = process.env.PORT || 5200;
 
 app.get("/", (req, res) => {
     res.send("Hello From the Server!");
