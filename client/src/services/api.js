4f346d1df145ca7f539916f2b69e34f21ab9e004
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2026-04-09 00:37:44 +0545

    chore: update API base URL to new render endpoint

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({ 
-    baseURL: "https://khata-saas-i94i.onrender.com/api"
+    baseURL: "https://khata-saas-nwg5.onrender.com/api"
 });
 
 // Add a request interceptor to include the JWT token in all requests
