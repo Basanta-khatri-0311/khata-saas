f50ef59ac8317f5bb62db821a82a6e0f6981ce02
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2026-04-08 23:24:24 +0545

    chore: update api base URL to production render endpoint

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const api = axios.create({
-    baseURL: "http://localhost:5500/api"
+const api = axios.create({ 
+    baseURL: "https://khata-saas-i94i.onrender.com"
 });
 
 // Add a request interceptor to include the JWT token in all requests
